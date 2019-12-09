a54ad54d0ccfac5f82e0f88ad1179da48a3889af
Belhadj <kaci_hadji@yahoo.fr>
2019-12-09 22:54:03 +0100

    Update home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,84 +1,148 @@
 import React, { Component } from "react";
-import { View, ScrollView } from "react-native";
-import FeedCard from "../Components/FeedCard";
-import {Container, Content} from "native-base";
-import firebase from "react-native-firebase";
+import { View, FlatList, ActivityIndicator, SafeAreaView } from "react-native";
+import { SearchBar } from "react-native-elements";
 import {getFrDate} from "../Utils/Functions";
+import FeedCard from "../Components/FeedCard";
+import firebase from "react-native-firebase";
 
 class HomeScreen extends Component {
     static navigationOptions = {
         header: null
     };
-  constructor(props) {
-    super(props);
-    this.state = {
-        annonces: []
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            loading: true,
+            annonces: [],
+            page: 1,
+            error: null,
+            refreshing: false
+        };
+    }
+
+    componentDidMount() {
+        this.loadAnnonces();
+    }
+
+    loadAnnonces = () => {
+        this.setState({ loading: true });
+        const that = this;
+        firebase
+            .firestore()
+            .collection("annonces")
+            // .where('enable', '==', true)
+            .orderBy('date', 'desc')
+            .limit(5)
+            .get()
+            .then(annonces => {
+
+                setTimeout(() => {
+                    const data = [];
+                    annonces.forEach(function (doc) {
+                        const row = doc.data();
+                        row.id = doc.id;
+                        data.push(row) ;
+                    });
+                that.setState({
+                    annonces: that.state.page === 1 ? data : [...that.state.annonces, ...data],
+                    loading: false,
+                    refrloadAnnonceseshing: false});
+                            }, 2000);
+
+            });
+
     };
-    const that = this;
 
-      firebase
-        .firestore()
-        .collection("annonces")
-        // .where('enable', '==', true)
-        .orderBy('date')
-        .limit(5)
-        .get()
-        .then(annonces => {
-        const data = [];
-        annonces.forEach(function (doc) {
-          data.push(doc.data()) ;
-      });
-        that.setState({annonces: data});
-    });
-  }
+    handleRefresh = () => {
+        this.setState(
+            {
+                page: 1,
+                refreshing: true
+            },
+            () => {
+                this.loadAnnonces();
+            }
+        );
+    };
 
-  showAnnonces = () => {
-      if (this.state.annonces.length > 0) {
-          return (
-              this.state.annonces.map((annonce, index) => {
-                    return <FeedCard
-                        key={index}
-                        title={annonce.title}
-                        date={getFrDate(new Date())}
-                        text={annonce.text}
-                        backgroundColor={this.isNewAnnonce(annonce) ? "#ffffff" : "#b3b3b3"}
-                        dateColor={this.isNewAnnonce(annonce) ? "#000000" : "#ffffff"}
-                    />
-              })
-          )
-      }
-      return null;
-  }
+    handleLoadMore = () => {
+        this.setState(
+            {
+                page: this.state.page + 1
+            },
+            () => {
+                this.loadAnnonces();
+            }
+        );
+    };
 
-  scroll = () => {
-      console.log('###Scroll###Scroll###Scroll###Scroll###Scroll###Scroll###Scroll###Scroll###Scroll###');
-        const annonces = that.state.annonces;
+    renderSeparator = () => {
+        return (
+            <View
+                style={{
+                    height: 1,
+                    width: "86%",
+                    backgroundColor: "#CED0CE",
+                    marginLeft: "14%"
+                }}
+            />
+        );
+    };
 
-      annonces.push({
-         title: "test test",
-         text: "test test  test  test  test  test  test  test  test  test  test  test  test ",
-      });
-      this.setState({annonces: data});
-  }
+    renderHeader = () => {
+        return <SearchBar placeholder="Type Here..." lightTheme round />;
+    };
 
-  isNewAnnonce =(annonce) => {
-      const now = new Date();
-      const today = new Date(now.getFullYear() + '-' + (now.getMonth()+1) + '-' + now.getDate());
-      return annonce.date.toDate() >= today;
-  }
-  render() {
-    return (
-        <ScrollView
-            onScrollEndDrag={this.scroll}
-            nestedScrollEnabled={true}
+    renderFooter = () => {
+        if (!this.state.loading) return null;
+        return (
+            <View
+                style={{
+                    marginBottom: 100,
+                    paddingVertical: 20,
+                    borderTopWidth: 1,
+                    borderColor: "#CED0CE"
+                }}
             >
-            <Container>
-            <Content>
-                {this.showAnnonces()}
-            </Content>
-            </Container>
-    </ScrollView>);
-
-  }
+                <ActivityIndicator animating size="large" />
+            </View>
+        );
+    };
+    isNewAnnonce =(annonce) => {
+        const now = new Date();
+        const today = new Date(now.getFullYear() + '-' +  ((parseInt(now.getMonth().toString()) + 1) + '') .padStart(2, "0") + '-' +  now.getDate().toString().padStart(2, "0") + 'T00:00:00');
+        return annonce.date.toDate() >= today;
+    }
+    renderItem = item => {
+        return (
+            <FeedCard
+                title={item.title}
+                date={getFrDate(item.date.toDate())}
+                text={item.text}
+                backgroundColor={this.isNewAnnonce(item) ? "#ffffff" : "#b3b3b3"}
+                dateColor={this.isNewAnnonce(item) ? "#000000" : "#ffffff"}
+            />
+        );
+    }
+    render() {
+        return (
+            <SafeAreaView >
+                <FlatList
+                    data={this.state.annonces}
+                    renderItem={({ item }) => this.renderItem(item)}
+                    keyExtractor={item => item.id}
+                    ItemSeparatorComponent={this.renderSeparator}
+                    ListHeaderComponent={this.renderHeader}
+                    ListFooterComponent={this.renderFooter}
+                    onRefresh={this.handleRefresh}
+                    refreshing={this.state.refreshing}
+                    onEndReached={this.handleLoadMore}
+                    onEndReachedThreshold={1}
+                />
+            </SafeAreaView>
+        );
+    }
 }
+
 export default HomeScreen;
