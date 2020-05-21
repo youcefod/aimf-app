1a9b98763957e6f077584adc4df0ced20db70496
belahdj <kaci_hadji@yahoo.fr>
2020-05-21 15:37:31 +0200

    Update notifications

--- src/Loading.js
+++ src/Loading.js
@@ -4,7 +4,6 @@ import { connect } from "react-redux";
 import * as PropTypes from "prop-types";
 import NavigationService from "./Utils/NavigationService";
 import { navigate } from "./Utils/Account";
-import firebase from "react-native-firebase";
 import Notifications from "./services/Notifications";
 
 const styles = StyleSheet.create({
@@ -19,42 +18,13 @@ class Loading extends React.Component {
   componentDidMount() {
     NavigationService.setInstance(this.props.navigation);
     navigate(this.props.account, this.props.navigation);
-      //new Notifications(this.onRegister.bind(this));
+    new Notifications(this.onRegister.bind(this));
   }
 
-    saveDeviceToken(token, userRefecrence) {
-        firebase
-            .firestore()
-            .collection("deviceToken")
-            .add({
-                value: token,
-                user: userRefecrence
-            })
-            .then(() => {
-            })
-            .catch(error => {
-            });
-    }
+  onRegister = (token) => {
+    console.log('######################## token : ', token);
+  };
 
-    onRegister(token) {
-        if (!this.state.tokenCreated) {
-            const userRefecrence = firebase.firestore().collection("users").doc(this.state.user._user.uid);
-            firebase
-                .firestore()
-                .collection("deviceToken")
-                .where('value', '==', token.token)
-                .where('user', '==', userRefecrence)
-                .get()
-                .then(tokens => {
-                    if (tokens.size == 0) {
-                        this.saveDeviceToken(token.token, userRefecrence);
-                    }
-                })
-                .catch(error => {
-                });
-        }
-        this.setState({tokenCreated: true});
-    }
   render() {
     return (
       <View style={styles.container}>
